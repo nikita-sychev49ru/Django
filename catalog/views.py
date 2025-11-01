ad8e7a5483451f9439916ac76aecfd4031cf363b
nikita <hesoyame1998@gmail.com>
2025-11-02 07:13:35 +1100

    add controllers and templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено и ваш номер телефона {phone} записан.")
+    return render(request, 'contacts.html')
