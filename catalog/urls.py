9b60f9487b4b63a10796634faeb03de781a0b599
nikita <hesoyame1998@gmail.com>
2025-11-04 04:13:41 +1100

    upadte views and urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_card, name='product_card'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context=context)
 
 def contacts(request):
     if request.method == 'POST':
@@ -14,3 +17,8 @@ def contacts(request):
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено и ваш номер телефона {phone} записан.")
     return render(request, 'contacts.html')
+
+def product_card(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'product_card.html', context=context)
