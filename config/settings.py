9fb51295c700db7a280d88f384527a3207f61fe9
nikita <hesoyame1998@gmail.com>
2025-11-08 21:12:49 +1100

    install redis

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,12 @@ EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') =='True' else False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'fullhp3248@mail.ru')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+        }
+    }
